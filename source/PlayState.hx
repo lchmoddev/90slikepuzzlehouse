7c802a00d06cf57cee747879e259ebf4e37d9c56
lchmoddev <79424337+lchmoddev@users.noreply.github.com>
2022-02-22 15:26:35 -0500

    cooldude

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,26 @@
 package;
 
+import flash.display.BitmapData;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var room:String = "Room2";
+	var stage:FlxSprite;
+
 	override public function create()
 	{
-		super.create();
+		updateroom(room);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+
+	private function updateroom(roomtogoto:String)
+	{
+		var stage = new FlxSprite(0, 0, BitmapData.fromFile("assets/images/room2_0"));
 	}
 }
